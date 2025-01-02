aeab8e4c1bd77b02c17d47bcf10ad99aace96230
AtomicAntt <anthonychi36@gmail.com>
2025-01-02 15:14:13 -0800

    Handle loading and unloading levels

--- main.gd
+++ main.gd
@@ -1,11 +1,21 @@
 extends Control
 
+@onready var world = $World
+var level_instance: Node2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	load_level("Level1")
 
+func unload_level() -> void:
+	if is_instance_valid(level_instance):
+		level_instance.queue_free()
+	level_instance = null
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func load_level(level_name: String) -> void:
+	unload_level()
+	var level_path: String = "res://Levels/%s.tscn" % level_name
+	var level_resource: Resource = load(level_path)
+	
+	if level_resource:
+		level_instance = level_resource.instantiate()
+		world.add_child(level_instance)
