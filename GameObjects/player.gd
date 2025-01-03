073226c7939361e5f56e26eba22b1c46bb82e27b
AtomicAntt <anthonychi36@gmail.com>
2025-01-02 16:45:59 -0800

    Initialize clock boss and make movement script into a state machine

--- GameObjects/player.gd
+++ GameObjects/player.gd
@@ -1,25 +1,38 @@
+class_name Player
 extends CharacterBody2D
 
+enum States {AIR, FLOOR, DEAD}
+var state: States = States.AIR
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -300.0
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+	fall(delta)
+	
+	match state:
+		States.AIR:
+			get_horizontal_movement()
+			if is_on_floor():
+				state = States.FLOOR
+		States.FLOOR:
+			get_horizontal_movement()
+			if Input.is_action_pressed("up"):
+				velocity.y += JUMP_VELOCITY
+				state = States.AIR
+		States.DEAD:
+			velocity.x = lerp(velocity.x, 0.0, 0.4)
+
+func get_horizontal_movement() -> void:
+	if Input.is_action_pressed("right"):
+		velocity.x = lerp(velocity.x, SPEED, 0.2)
+	elif Input.is_action_pressed("left"):
+		velocity.x = lerp(velocity.x, -SPEED, 0.2)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, 0.4)
+
+func fall(delta: float) -> void:
+	velocity.y += gravity * delta
