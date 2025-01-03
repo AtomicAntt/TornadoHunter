3413640a70870895fcc63630b0922d73e73c7b6d
AtomicAntt <anthonychi36@gmail.com>
2025-01-02 19:31:52 -0800

    Add some player animation

--- GameObjects/player.gd
+++ GameObjects/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 enum States {NORMAL, DEAD}
 var state: States = States.NORMAL
 
-@export var SPEED = 200.0
+@export var SPEED: float = 200.0
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
@@ -11,6 +11,7 @@ func _physics_process(delta: float) -> void:
 	match state:
 		States.NORMAL:
 			get_basic_movement()
+			look_at_mouse()
 		States.DEAD:
 			velocity = Vector2.ZERO
 
@@ -26,4 +27,17 @@ func get_basic_movement() -> void:
 	if Input.is_action_pressed("down"):
 		velocity.y = 1
 	
+	if velocity.length() > 0.0:
+		$AnimatedSprite2D.play("Run")
+	else:
+		$AnimatedSprite2D.play("Idle")
+	
 	velocity = velocity * SPEED
+
+func look_at_mouse() -> void:
+	var pos: Vector2 = get_local_mouse_position()
+	if pos.x < 0.0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+	
