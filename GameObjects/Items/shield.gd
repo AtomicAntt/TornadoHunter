c7a995c1f8653f4c5afce2cba60d62f21e503795
AtomicAntt <anthonychi36@gmail.com>
2025-01-03 21:30:24 -0800

    Shields recharge after use

--- GameObjects/Items/shield.gd
+++ GameObjects/Items/shield.gd
@@ -1,7 +1,19 @@
 extends Area2D
 
+var max_recharge: float = 5.0
+var recharge: float = 5.0
+
+func _physics_process(delta: float) -> void:
+	if recharge < max_recharge:
+		recharge += delta
+		$AnimatedSprite2D.modulate = Color("#ff312a")
+		modulate.a = recharge / max_recharge
+	else:
+		$AnimatedSprite2D.modulate = Color("#ffffff")
+
 func _on_area_entered(area: Area2D) -> void:
-	if area.is_in_group("EnemyAttack"):
+	if area.is_in_group("EnemyAttack") && recharge >= max_recharge:
 		var enemyAttack: EnemyProjectile = area
-		area.queue_free()
-		queue_free()
+		enemyAttack.queue_free()
+		recharge = 0.0
+		
