6a23aa9143ac9be43e6b4d98198f837d31861410
AtomicAntt <anthonychi36@gmail.com>
2025-01-06 16:42:11 -0800

    Add charging AI for the tumbleweed boss.

--- GameObjects/Bosses/Tumbleweed.gd
+++ GameObjects/Bosses/Tumbleweed.gd
@@ -4,7 +4,7 @@ enum States {STUNNED, ACCELERATING, CHARGING, IDLE, DEAD}
 var state: States = States.STUNNED
 
 var spin_speed: float = 0.0
-@export var spin_acceleration: float = 100.0
+@export var spin_acceleration: float = 200.0
 @export var speed_to_charge: float = 1000.0
 
 var last_player_position: Vector2 = Vector2.ZERO
@@ -17,6 +17,9 @@ func _physics_process(delta: float) -> void:
 			spin_speed += spin_acceleration * delta
 			rotation_degrees += spin_speed * delta
 			
+			while rotation_degrees > 360:
+				rotation_degrees -= 360
+			
 			if spin_speed >= speed_to_charge:
 				state = States.CHARGING
 				var player: Player = get_tree().get_nodes_in_group("Player")[0]
@@ -27,10 +30,28 @@ func _physics_process(delta: float) -> void:
 			rotation_degrees -= spin_speed * delta
 			
 			global_position += global_position.direction_to(last_player_position) * spin_speed * delta
+		States.STUNNED:
+			pass
 
 func _on_intro_timeout() -> void:
-	state = States.ACCELERATING
-
+	accelerate()
 
 func _on_body_entered(body: Node2D) -> void:
-	spin_speed = 0
+	if state == States.CHARGING:
+		stun()
+		spin_speed = 0
+		$StunTimer.start()
+
+func stun() -> void:
+	state = States.STUNNED
+	$StunAnimation.visible = true
+	$StunAnimation.play("Stunned")
+
+func accelerate() -> void:
+	state = States.ACCELERATING
+	$StunAnimation.visible = false
+	$StunAnimation.stop()
+
+func _on_stun_timer_timeout() -> void:
+	print("READY TO ROLL")
+	accelerate()
