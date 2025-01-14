9659f72a38e9f4b583475fcbf465e07fdd3f64e8
AtomicAntt <anthonychi36@gmail.com>
2025-01-13 22:21:34 -0800

    Make cutscene 1 second faster and if the player is at the gate already, let them in.

--- GameObjects/Environment/gate.gd
+++ GameObjects/Environment/gate.gd
@@ -22,10 +22,10 @@ func cutscene() -> void:
 	tween.tween_property(cutscene_camera, "global_position", self.global_position, 1)
 	
 	await tween.finished
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(0.5).timeout
 	$Sprite2D.visible = false
 	$CollisionShape2D.set_deferred("disabled", true)
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(0.5).timeout
 	
 	tween = create_tween()
 	tween.tween_property(cutscene_camera, "global_position", current_camera.global_position, 1)
@@ -36,6 +36,15 @@ func cutscene() -> void:
 	cutscene_camera.enabled = false
 	
 	open = true
+	
+	# If the player is standing next to the gate already
+	
+	for area in $Area2D.get_overlapping_areas():
+		if area.is_in_group("PlayerHitbox"):
+			open = false
+			var main: Main = get_tree().get_nodes_in_group("Main")[0]
+			if not is_shop:
+				main.load_level("Shop")
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("PlayerHitbox") and open:
