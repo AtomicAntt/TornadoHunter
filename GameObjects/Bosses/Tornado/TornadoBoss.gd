711768c110bd63821db7f946e18e198d91c1a09c
AtomicAntt <anthonychi36@gmail.com>
2025-01-15 21:48:51 -0800

    Initialize values for spinning wind attack and wind attack for the 2nd boss.

--- GameObjects/Bosses/Tornado/TornadoBoss.gd
+++ GameObjects/Bosses/Tornado/TornadoBoss.gd
@@ -1,1 +1,7 @@
 extends Boss
+
+var spinning_wind: Resource = preload("res://GameObjects/Bosses/Tornado/SpinningWind.tscn")
+var wind_attack: Resource = preload("res://GameObjects/Bosses/Tornado/TornadoWind.tscn")
+
+func _physics_process(delta: float) -> void:
+	pass
