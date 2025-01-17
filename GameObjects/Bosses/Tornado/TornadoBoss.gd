a2296f5f4b06c191f48bb6914929bfefca9ef1f1
AtomicAntt <anthonychi36@gmail.com>
2025-01-17 15:20:17 -0800

    Create the tornado spiral attack

--- GameObjects/Bosses/Tornado/TornadoBoss.gd
+++ GameObjects/Bosses/Tornado/TornadoBoss.gd
@@ -3,21 +3,54 @@ extends Boss
 var spinning_wind: Resource = preload("res://GameObjects/Bosses/Tornado/SpinningWind.tscn")
 var wind_attack: Resource = preload("res://GameObjects/Bosses/Tornado/TornadoWind.tscn")
 
-var attack_cooldown: float = 2.0
+var attack_cooldown: float = 0.1
 var attack_time: float = 0.0
 
+# This is for when the boss is in idle, how much time needed until charging again?
+var charge_cooldown: float = 2.0
+var charge_time: float = 0.0
+
+var rotation_speed: float = 100
+var spawn_point_count: int = 4
+var radius: float = 10
+
+var speed: float = 100
+
+var movement_tween: Tween
+
+var position_index: int = 0 # Basically this loops through the positions of 0, 1, and 2 using the modulo operator
+
+func _ready() -> void:
+	var step: float = 2 * PI / spawn_point_count
+	
+	for i in range(spawn_point_count):
+		var spawn_point: Node2D = Node2D.new()
+		var pos: Vector2 = Vector2(radius, 0).rotated(step * i)
+		spawn_point.position = pos
+		spawn_point.rotation = pos.angle()
+		$Rotater.add_child(spawn_point)
+
 func _physics_process(delta: float) -> void:
+	var new_rotation = $Rotater.rotation_degrees + rotation_speed * delta
+	$Rotater.rotation_degrees = fmod(new_rotation, 360)
+	
 	match state:
 		States.IDLE:
-			pass
+			charge_time += delta
+			if charge_time >= charge_cooldown:
+				charge_time = 0.0
+				set_charging()
 		States.CHARGING:
 			attack_time += delta
 			if attack_time >= attack_cooldown:
 				attack_time = 0.0
-				shoot_spinning_wind()
+				shoot_spiral()
 			
+			if not movement_tween.is_running():
+				set_idle()
+
 func _on_intro_timeout() -> void:
-	state = States.CHARGING
+	set_charging()
 
 func shoot_spinning_wind() -> void:
 	var player: Player = get_tree().get_nodes_in_group("Player")[0]
@@ -28,9 +61,38 @@ func shoot_spinning_wind() -> void:
 	wind_instance.global_position = global_position
 	get_parent().call_deferred("add_child", wind_instance)
 
+func shoot_spiral() -> void:
+	for s: Node2D in $Rotater.get_children():
+		var wind_instance: EnemyProjectile = wind_attack.instantiate()
+		wind_instance.global_position = s.global_position
+		wind_instance.set_direction(Vector2.RIGHT.rotated(s.global_rotation))
+		wind_instance.set_speed(80)
+		get_parent().add_child(wind_instance)
+
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("PlayerHitbox"):
 		var player: Player = get_tree().get_nodes_in_group("Player")[0]
 		if is_instance_valid(player):
 			if !player.is_invulnerable():
 				player.hurt(1)
+
+func set_charging() -> void:
+	state = States.CHARGING
+	charge_time = 0
+	
+	visit_new_marker()
+
+func visit_new_marker() -> void:
+	position_index += 1
+	var new_marker: Marker2D
+	
+	# position_index % 3 basically, which loops from 0, 1, and 2
+	new_marker = get_tree().get_nodes_in_group("BossPosition")[position_index % get_tree().get_node_count_in_group("BossPosition")]
+	
+	if is_instance_valid(new_marker):
+		movement_tween = get_tree().create_tween()
+		movement_tween.tween_property(self, "global_position", new_marker.global_position, global_position.distance_to(new_marker.global_position) / speed)
+
+func set_idle() -> void:
+	state = States.IDLE
+	attack_time = 0
