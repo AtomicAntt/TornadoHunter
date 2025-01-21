4681ee6b0fce8f2200bad5afc2f53349f0568326
AtomicAntt <anthonychi36@gmail.com>
2025-01-21 12:29:55 -0800

    Rework the logic of the third boss.

--- GameObjects/Bosses/Clock/ClockBoss.gd
+++ GameObjects/Bosses/Clock/ClockBoss.gd
@@ -7,8 +7,11 @@ var new_marker: Marker2D
 var move_cooldown: float = 2.0 # Time it takes before it takes an action and gets out of idle state
 var move_time: float = 0.0
 
-var shooting_cooldown: float = 3.0 # Time it takes before it goes back into being idle again
+var shooting_cooldown: float = 5.0 # Time it takes before it goes back into being idle again
 var shooting_time: float = 0.0
+
+var fire_cooldown: float = 0.2 # Time it takes to fire a spinning clock projectile
+var fire_time: float = 0.0
 
 var small_clock_spinner: Resource = preload("res://GameObjects/Bosses/Clock/SmallClockSpinner.tscn")
 var clock_spinner: Resource = preload("res://GameObjects/Bosses/Clock/ClockSpinner.tscn")
@@ -22,27 +25,32 @@ func _physics_process(delta: float) -> void:
 				move_time += delta
 			
 			if move_time >= move_cooldown:
-				move_time = 0.0
 				set_moving()
-		States.MOVING:
+		States.MOVING: # This state is done as an intermission to the shooting state. When the animationplayer for this finishes, it will switch to the shooting state.
 			pass
-		States.SHOOTING: # Goes into shooting state once it shoots clocks. When it does that, it will wait and then go back to idle state in this logic below.
+		States.SHOOTING: # Goes into shooting state once it shoots clocks. When it does that, it will shoot them and then go back to idle state in this logic below.
 			shooting_time += delta
+			fire_time += delta
+			
+			if fire_time >= fire_cooldown:
+				fire_time = 0.0
+				summon_clock()
 			
 			if shooting_time >= shooting_cooldown:
-				shooting_time = 0.0
 				set_idle()
 
 func set_idle() -> void:
 	if state != States.DISABLED:
 		state = States.IDLE
+		
+		# Reset the times needed to switch to idle and to shoot a clock projectile.
+		shooting_time = 0.0
+		fire_time = 0.0
 
 func set_moving() -> void:
 	state = States.MOVING
+	move_time = 0.0 # Reset time needed to enter the moving state from idle state.
 	$TeleportAnimation.play("Teleport")
-
-func set_shooting() -> void:
-	state = States.SHOOTING
 
 func set_new_marker() -> void:
 	position_index += 1
@@ -63,15 +71,7 @@ func get_random_projectile_position(current_position: int) -> Vector2:
 	print("ERROR: current_position of get_random_projectile_position is not a valid number!")
 	return Vector2.ZERO
 
-# ====================================== METHODS CALLED BY ANIMATIONPLAYER ================================
-
-# This will be called by the DeathAnimation
-func visit_new_marker() -> void:
-	set_new_marker()
-	global_position = new_marker.global_position
-
-# This will be called by the DeathAnimation
-func summon_clocks() -> void:
+func summon_clock() -> void:
 	var direction_to_shoot: Vector2
 	var current_position: int = position_index % get_tree().get_node_count_in_group("BossPosition")
 	
@@ -83,32 +83,40 @@ func summon_clocks() -> void:
 		2: # Right Location
 			direction_to_shoot = Vector2.LEFT
 	
-	for i in range(2):
+	var random_float = randf()
+	if random_float < 0.2:
 		var big_spinner_instance: ClockSpinner = big_clock_spinner.instantiate()
 		big_spinner_instance.set_direction(direction_to_shoot)
 		big_spinner_instance.global_position = get_random_projectile_position(current_position)
 		get_parent().add_child(big_spinner_instance)
-	
-	for i in range(2):
+	elif random_float < 0.4:
 		var medium_spinner_instance: ClockSpinner = medium_clock_spinner.instantiate()
 		medium_spinner_instance.set_direction(direction_to_shoot)
 		medium_spinner_instance.global_position = get_random_projectile_position(current_position)
 		get_parent().add_child(medium_spinner_instance)
-	
-	for i in range(2):
+	elif random_float < 0.6:
 		var spinner_instance: ClockSpinner = clock_spinner.instantiate()
 		spinner_instance.set_direction(direction_to_shoot)
 		spinner_instance.global_position = get_random_projectile_position(current_position)
 		get_parent().add_child(spinner_instance)
-	
-	for i in range(4):
+	elif random_float >= 0.6 and random_float <= 1.0:
 		var small_spinner_instance: ClockSpinner = small_clock_spinner.instantiate()
 		small_spinner_instance.set_direction(direction_to_shoot)
 		small_spinner_instance.global_position = get_random_projectile_position(current_position)
 		get_parent().add_child(small_spinner_instance)
+	else:
+		print("ERROR: could not spawn a clock instance.")
 	
-	
-	set_shooting() # This is so that there is some wait time before it swithces back to an idle state.
+# ====================================== METHODS CALLED BY ANIMATIONPLAYER ================================
+
+# This will be called by TeleportAnimation
+func visit_new_marker() -> void:
+	set_new_marker()
+	global_position = new_marker.global_position
+
+# This will be called by TeleportAnimation
+func set_shooting() -> void:
+	state = States.SHOOTING
 
 # ====================================== SIGNAL METHODS =================================
 
