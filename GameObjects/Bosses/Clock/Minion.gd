9a3ff837d2f8c5be15cdadf92737cf5662aa76b7
AtomicAntt <anthonychi36@gmail.com>
2025-01-22 21:30:12 -0800

    Allow hourglass minion to move randomly through the arena.

--- GameObjects/Bosses/Clock/Minion.gd
+++ GameObjects/Bosses/Clock/Minion.gd
@@ -1,14 +1,44 @@
 class_name Minion
 extends Area2D
 
-@export var minion_max_health = 100
-@export var minion_health = 100
+@export var minion_max_health: float = 100
+@export var minion_health: float = 100
 
 @onready var progress_under: TextureProgressBar = $HealthBar/DamageBar
-@export var duration: float = 0.3
+var duration: float = 0.3
 
-enum States {DISABLED, IDLE, CHARGING, STUNNED}
+@export var speed: float = 100
+
+var current_pos: Marker2D
+
+var movement_tween: Tween
+
+var move_cooldown: float = 3.0 # Time it takes before it takes an action and gets out of idle state
+var move_time: float = 0.0
+
+enum States {DISABLED, IDLE, CHARGING, STUNNED, MOVING, WARNING}
 var state: States = States.IDLE
+
+func _physics_process(delta: float) -> void:
+	match state:
+		States.IDLE:
+			move_time += delta
+			
+			if move_time >= move_cooldown:
+				move_randomly() # Also resets move time
+		States.DISABLED: # Dead
+			pass
+		States.CHARGING:
+			pass
+		States.STUNNED:
+			pass
+		States.MOVING: # When moving is set, a tween is ran. When the tween is no longer running, the minion has reached its destination and will send a warning to charge at the player.
+			if is_instance_valid(movement_tween):
+				if not movement_tween.is_running():
+					set_idle()
+
+func set_idle() -> void:
+	state = States.IDLE
 
 func change_health(max_health: float, health: float) -> void:
 	var new_value: float = (health/max_health) * 100
@@ -36,3 +66,17 @@ func death() -> void:
 		
 		$DeathAnimation.play("Death")
 		$Death.play()
+
+func move_randomly() -> void:
+	move_time = 0.0
+	
+	var valid_positions: Array[Marker2D] = []
+	for random_pos: Marker2D in get_tree().get_nodes_in_group("MinionPosition"):
+		if random_pos != current_pos:
+			valid_positions.append(random_pos)
+	
+	var selected_marker: Marker2D = valid_positions.pick_random()
+	current_pos = selected_marker
+	
+	movement_tween = get_tree().create_tween()
+	movement_tween.tween_property(self, "global_position", selected_marker.global_position, global_position.distance_to(selected_marker.global_position) / speed).set_trans(Tween.TRANS_CUBIC)
